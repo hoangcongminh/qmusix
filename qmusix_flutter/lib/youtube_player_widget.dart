04bb15c13a419908f2f03ee6498da556efcd7eeb
congminh <congminh2320@gmail.com>
2023-10-22 20:54:54 +0700

    init flutter project

---
+++ qmusix_flutter/lib/youtube_player_widget.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
+
+class YoutubePlayerWidget extends StatefulWidget {
+  const YoutubePlayerWidget({super.key});
+
+  @override
+  State<YoutubePlayerWidget> createState() => _YoutubePlayerWidgetState();
+}
+
+class _YoutubePlayerWidgetState extends State<YoutubePlayerWidget> {
+  YoutubePlayerController? _youtubeController;
+
+  @override
+  void initState() {
+    super.initState();
+    _youtubeController = YoutubePlayerController.fromVideoId(
+      videoId: 'L_z9YHX5ous',
+      autoPlay: true,
+      params: const YoutubePlayerParams(
+        showControls: false,
+        pointerEvents: PointerEvents.none,
+      ),
+    )..stream.listen(
+        (event) {
+          if (event.playerState == PlayerState.ended) {
+            _youtubeController!.loadVideoById(
+              videoId: 'mbEZ_9dhM_Y',
+            );
+          }
+        },
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayer(
+      controller: _youtubeController!,
+      aspectRatio: 16 / 9,
+    );
+  }
+}
