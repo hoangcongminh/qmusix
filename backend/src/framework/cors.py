7a5dc23e256d6d415924c31c7305d8e60df7f8f6
hnam <hoangnam021200@gmail.com>
2023-11-18 12:19:36 +0700

    Cors allow all method

--- backend/src/framework/cors.py
+++ backend/src/framework/cors.py
@@ -1,5 +1,7 @@
-from flask_cors import CORS
-
-
-def cors(app):
-    CORS(app)
+def cors_config(app):
+    @app.after_request
+    def after_request(response):
+        response.headers.add("Access-Control-Allow-Methods", "*")
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        return response
