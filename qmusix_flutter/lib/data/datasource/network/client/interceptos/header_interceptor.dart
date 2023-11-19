8037b9dc4fa6ed3c0908d28e44220f0d0741eec2
congminh <congminh2320@gmail.com>
2023-11-19 14:05:05 +0700

    fix cors & implement register page

--- qmusix_flutter/lib/data/datasource/network/client/interceptos/header_interceptor.dart
+++ qmusix_flutter/lib/data/datasource/network/client/interceptos/header_interceptor.dart
@@ -4,10 +4,9 @@ class HeaderInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     options.headers.addAll({
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+      //   "Access-Control-Allow-Origin": "*",
+      //   "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+      // "Access-Control-Allow-Headers": "*",
     });
     super.onRequest(options, handler);
   }
--- qmusix_flutter/lib/presentation/register/register_page.dart
+++ qmusix_flutter/lib/presentation/register/register_page.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:qmusix/domain/repository/auth_repository.dart';
+import 'package:qmusix/injection.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +19,36 @@ class RegisterPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Register Page"),
       ),
-      body: const Center(
-        child: Text("Register Page"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: "Username",
+              ),
+            ),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Password",
+              ),
+            ),
+            FilledButton(
+              onPressed: () {
+                // context.push("/home");
+                AppInjection.get<AuthRepository>().register(
+                  username: _usernameController.text,
+                  password: _passwordController.text,
+                );
+              },
+              child: const Text("Register"),
+            ),
+          ],
+        ),
       ),
     );
   }
